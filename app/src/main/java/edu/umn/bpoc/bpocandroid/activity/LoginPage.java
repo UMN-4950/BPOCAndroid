2864bf0d35d138c0198a5dee16b8d53740389508
Hooman Yar <hooman.yar@gmail.com>
2017-01-24 21:43:48 -0600

    handling signIn result

--- app/src/main/java/edu/umn/bpoc/bpocandroid/activity/LoginPage.java
+++ app/src/main/java/edu/umn/bpoc/bpocandroid/activity/LoginPage.java
@@ -1,26 +1,35 @@
 package edu.umn.bpoc.bpocandroid.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 import edu.umn.bpoc.bpocandroid.R;
+import edu.umn.bpoc.bpocandroid.Util;
 
 public class LoginPage extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener,
         View.OnClickListener {
 
+    private ProgressDialog mProgressDialog;
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 9001;
+    private static final String TAG = "SignInActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,13 @@ public class LoginPage extends AppCompatActivity implements
         setContentView(R.layout.activity_login_page);
 
         // dev sign-in button
-        signInDev();
+        Button dev_sign_in = (Button)findViewById(R.id.button_optional_action);
+        dev_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInToMapView();
+            }
+        });
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
@@ -57,20 +72,37 @@ public class LoginPage extends AppCompatActivity implements
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> optionalPendingResult = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (optionalPendingResult.isDone()) {
+            // returning user who has been granted the authorization
+            Log.d(TAG, "Cached sign-in");
+            GoogleSignInResult result = optionalPendingResult.get();
+            handleSignInResult(result);
+        }
+        else {
+            // if previous sign in has expired
+            showProgressDialog();
+            optionalPendingResult.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
     /**
      * development sign-in without authentication process
      */
-    public void signInDev() {
-    Button dev_sign_in = (Button)findViewById(R.id.button_optional_action);
-    dev_sign_in.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(LoginPage.this,MapsActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    });
-}
+    public void signInToMapView() {
+        Intent intent = new Intent(LoginPage.this,MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
@@ -79,11 +111,61 @@ public class LoginPage extends AppCompatActivity implements
 
     @Override
     public void onClick(View v) {
-        signIn();
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            default:
+                Util.generateToast("default button", getApplicationContext());
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            assert acct != null;
+            String displayName = acct.getDisplayName();
+            String userId = acct.getId();
+            String userEmail = acct.getEmail();
+
+            signInToMapView();
+        }
+        else {
+            Util.generateToast("didn't sign in", getApplicationContext());
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
     }
 }
