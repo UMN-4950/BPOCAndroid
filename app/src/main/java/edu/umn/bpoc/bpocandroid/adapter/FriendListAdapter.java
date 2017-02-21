a3097d63569c3fa121e9e968a52c1e58a4f687b9
Ninjia872 <wz.nicky@gmail.com>
2017-02-21 11:38:08 -0600

    fake friend list, need fake data and adapter

---
+++ app/src/main/java/edu/umn/bpoc/bpocandroid/adapter/FriendListAdapter.java
@@ -0,0 +1,9 @@
+package edu.umn.bpoc.bpocandroid.adapter;
+
+/**
+ * Created by wznic on 2/17/2017.
+ */
+
+public class FriendListAdapter {
+//customer adapter for list view
+}
--- app/src/main/java/edu/umn/bpoc/bpocandroid/fragment/FriendListFragment.java
+++ app/src/main/java/edu/umn/bpoc/bpocandroid/fragment/FriendListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import edu.umn.bpoc.bpocandroid.R;
@@ -16,16 +17,20 @@ import edu.umn.bpoc.bpocandroid.R;
 
 public class FriendListFragment extends Fragment {
     public FriendListFragment(){}
+    private ListView friendList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.content_friend_list, container, false);
+        //Toast for testing
         Context context = getActivity();
         CharSequence text = "Hello Friend List!";
         int duration = Toast.LENGTH_SHORT;
-
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+
+        friendList = (ListView)rootView.findViewById(R.id.friend_list);
+        //friendList.setAdapter(new);
         return rootView;
     }
 
