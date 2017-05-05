8a23ef7bffb8b4996dc72b307a8fddbc2488f597
Ninjia872 <wz.nicky@gmail.com>
2017-05-05 13:24:29 -0500

    waiting for the search api to fetch data

--- app/src/main/java/edu/umn/bpoc/bpocandroid/model/AddFriend.java
+++ app/src/main/java/edu/umn/bpoc/bpocandroid/model/AddFriend.java
@@ -2,19 +2,23 @@
 package edu.umn.bpoc.bpocandroid.model;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import edu.umn.bpoc.bpocandroid.R;
 
 public class AddFriend extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,22 @@ public class AddFriend extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        SearchView searchBar = (SearchView)findViewById(R.id.new_friend_search);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(getApplicationContext(), query, duration);
+                toast.show();
+                //insert the search api here, search name by the query
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     @Override
